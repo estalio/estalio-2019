4f37934840c5bee9fa4e0702cf56a8cefe69985a
Ruslan Bredikhin <rus.brn@gmail.com>
2020-01-15 08:04:57 -0500

    add some setup to deploy to now v2.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
 import svelte from 'rollup-plugin-svelte';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
@@ -23,6 +24,7 @@ export default {
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
+			json(),
 			svelte({
 				dev,
 				hydratable: true,
